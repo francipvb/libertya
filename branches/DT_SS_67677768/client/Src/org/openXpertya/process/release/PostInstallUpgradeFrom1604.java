7203ce1d8720d33dbb1cf7fbda2b62ab0b7698fa
fcristina <fcristina@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2016-06-15 17:22:31 +0000

    Actualizacion del branch con trunk, de r1480 a r1488

--- branches/DT_SS_67677768/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1604.java
+++ branches/DT_SS_67677768/client/Src/org/openXpertya/process/release/PostInstallUpgradeFrom1604.java
@@ -1,5 +1,6 @@
 package org.openXpertya.process.release;
 
+import org.openXpertya.JasperReport.MJasperReport;
 import org.openXpertya.model.MProcess;
 import org.openXpertya.process.PluginPostInstallProcess;
 import org.openXpertya.utils.JarHelper;
@@ -9,7 +10,7 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 	/** Reporte de Cierre de Tarjetas */
 	protected final static String CREDITCARD_CLOSE_REPORT_UID = "CORE-AD_Process-1010444";
 	protected final static String DYNAMIC_CREDITCARD_CLOSE_REPORT_UID = "CORE-AD_Process-1010445";
-	protected final static String CREDITCARD_CLOSE_REPORT_FILENAME = "CreditCardCloseReport.jrxml";
+	protected final static String CREDITCARD_CLOSE_REPORT_FILENAME = "CreditCardClose.jrxml";
 	
 	/** Reporte de Cierre de Tarjetas - Subreporte de Cupones Duplicados */
 	protected final static String CREDITCARD_CLOSE_SB_DUPLICATEDCUPON_REPORT_FILENAME = "CreditCardReport-DuplicateSubreport.jrxml";
@@ -20,6 +21,26 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 	/** Reporte de Auditoría de Cierre de Tarjetas */ 
 	protected final static String CLOSING_AUDIT_COUPONCARDS_REPORT_UID = "CORE-AD_Process-1010446";
 	protected final static String CLOSING_AUDIT_COUPONCARDS_REPORT_FILENAME = "ClosingAuditCouponCards.jrxml";
+	
+	/** UID del reporte del Orden de Pago*/
+	protected final static String ORDEN_PAGO_JASPER_REPORT_UID = "CORE-AD_JasperReport-1000012";
+	protected final static String ORDEN_PAGO_JASPER_REPORT_FILENAME = "OrdenPago.jasper";
+	
+	/** Informe de Recibos de Cliente */
+	protected final static String CUSTOMER_RECEIPT_JASPER_REPORT_UID = "CORE-AD_JasperReport-1000016";
+	protected final static String CUSTOMER_RECEIPT_JASPER_REPORT_FILENAME = "ReciboCliente.jasper";
+	
+	/** Informe de Recibos de Cliente - ReciboCliente-OtrosMedios */
+	protected final static String CUSTOMER_RECEIPT_OTHERPAYMENTS_JASPER_REPORT_UID = "CORE-AD_JasperReport-1000019";
+	protected final static String CUSTOMER_RECEIPT_OTHERPAYMENTS_JASPER_REPORT_FILENAME = "ReciboCliente_OtherPayments.jasper";
+	
+	/** Informe de Recibos de Cliente - ReciboCliente-NotasDeCredito */
+	protected final static String CUSTOMER_RECEIPT_CREDITNOTES_JASPER_REPORT_UID = "CORE-AD_JasperReport-1010132";
+	protected final static String CUSTOMER_RECEIPT_CREDITNOTES_JASPER_REPORT_FILENAME = "ReciboCliente-NotasDeCredito.jasper";
+	
+	/** IVA Ventas General */
+	protected final static String IVA_VENTA_GENERAL_REPORT_UID = "CORE-AD_Process-1010324";
+	protected final static String IVA_VENTA_GENERAL_REPORT_FILENAME = "Iva_Ventas.jrxml";
 	
 	@Override
 	protected String doIt() throws Exception {
@@ -64,7 +85,7 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 				JarHelper
 						.readBinaryFromJar(
 								jarFileURL,
-								getBinaryFileURL(CREDITCARD_CLOSE_REPORT_FILENAME)));
+								getBinaryFileURL(CREDITCARD_CLOSE_SB_DUPLICATEDCUPON_REPORT_FILENAME)));
 		
 		// Reporte de Cierre de Tarjetas - Subreporte de Cupones Duplicados
 		MProcess.addAttachment(
@@ -75,7 +96,7 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 				JarHelper
 						.readBinaryFromJar(
 								jarFileURL,
-								getBinaryFileURL(CREDITCARD_CLOSE_REPORT_FILENAME)));
+								getBinaryFileURL(CREDITCARD_CLOSE_SB_DUPLICATEDCUPON_REPORT_FILENAME)));
 		
 		// Reporte de Cierre de Tarjetas - Subreporte de Cupones Inválidos
 		MProcess.addAttachment(
@@ -86,7 +107,7 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 				JarHelper
 						.readBinaryFromJar(
 								jarFileURL,
-								getBinaryFileURL(CREDITCARD_CLOSE_REPORT_FILENAME)));
+								getBinaryFileURL(CREDITCARD_CLOSE_SB_INVALIDCUPON_REPORT_FILENAME)));
 		
 		// Reporte de Cierre de Tarjetas - Subreporte de Cupones Inválidos
 		MProcess.addAttachment(
@@ -97,7 +118,7 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 				JarHelper
 						.readBinaryFromJar(
 								jarFileURL,
-								getBinaryFileURL(CREDITCARD_CLOSE_REPORT_FILENAME)));
+								getBinaryFileURL(CREDITCARD_CLOSE_SB_INVALIDCUPON_REPORT_FILENAME)));
 		
 		// Reporte de Auditoría de Cierre de Tarjetas
 		MProcess.addAttachment(
@@ -110,7 +131,57 @@ public class PostInstallUpgradeFrom1604 extends PluginPostInstallProcess {
 								jarFileURL,
 								getBinaryFileURL(CLOSING_AUDIT_COUPONCARDS_REPORT_FILENAME)));
 		
+		// Informe de Orden de Pago
+		MJasperReport.updateBinaryData(get_TrxName(), getCtx(),
+				ORDEN_PAGO_JASPER_REPORT_UID, JarHelper.readBinaryFromJar(
+						jarFileURL,
+						getBinaryFileURL(ORDEN_PAGO_JASPER_REPORT_FILENAME)));
+		
+		// Actualización del reporte de Recibos de cliente
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					CUSTOMER_RECEIPT_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(CUSTOMER_RECEIPT_JASPER_REPORT_FILENAME)));
+		
+		// Actualización del reporte de Recibos de cliente - Otros Medios
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					CUSTOMER_RECEIPT_OTHERPAYMENTS_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(CUSTOMER_RECEIPT_OTHERPAYMENTS_JASPER_REPORT_FILENAME)));
+		
+		// Reporte de Recibos de cliente - Notas de Crédito
+		MJasperReport
+			.updateBinaryData(
+					get_TrxName(),
+					getCtx(),
+					CUSTOMER_RECEIPT_CREDITNOTES_JASPER_REPORT_UID,
+					JarHelper
+							.readBinaryFromJar(
+									jarFileURL,
+									getBinaryFileURL(CUSTOMER_RECEIPT_CREDITNOTES_JASPER_REPORT_FILENAME)));
+		
+		// Actualización del reporte Iva Ventas
+		MProcess.addAttachment(
+				get_TrxName(),
+				getCtx(),
+				IVA_VENTA_GENERAL_REPORT_UID,
+				IVA_VENTA_GENERAL_REPORT_FILENAME,
+				JarHelper
+						.readBinaryFromJar(
+								jarFileURL,
+								getBinaryFileURL(IVA_VENTA_GENERAL_REPORT_FILENAME)));
+				
 		return " ";
 	}
-
+	
 }
--- branches/DT_SS_67677768/data/core/upgrade_from_16.04/preinstall_from_16.04.sql
+++ branches/DT_SS_67677768/data/core/upgrade_from_16.04/preinstall_from_16.04.sql
@@ -221,7 +221,7 @@ CREATE TABLE c_bpartner_banklist (
   c_doctype_id integer NOT NULL,
   c_bpartner_id integer NOT NULL,
   nombre_retirante character varying(30),
-  c_bankaccount_id integer
+  c_bankaccount_id integer,
   CONSTRAINT c_bpartner_banklist_key PRIMARY KEY (c_bpartner_banklist_id)
 );
 ALTER TABLE c_bpartner_banklist
@@ -380,3 +380,6 @@ WITH (
 );
 ALTER TABLE C_CreditCard_CloseLine
   OWNER TO libertya;
+
+-- 20160610-2110 Nueva columna para permitir reutilizar Nro. de Documento en Recibos.
+update ad_system set dummy = (SELECT addcolumnifnotexists('C_Doctype','ReuseDocumentNo','character(1) default ''N''::bpchar'));
