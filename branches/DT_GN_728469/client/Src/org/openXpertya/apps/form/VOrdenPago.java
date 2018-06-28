094c5ee098709e5b159b9981809296b304e1be19
pablogeneos <pablogeneos@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2018-06-28 15:29:43 +0000

    Geneos - Tareas relacionadas al Pack04 * Pagos. No Permitir OP a EC No Marcadas como Proveedor * Cuentas a Pagar. Mensaje de Error "Almacén Org *" en FP

--- branches/DT_GN_728469/client/Src/org/openXpertya/apps/form/VOrdenPago.java
+++ branches/DT_GN_728469/client/Src/org/openXpertya/apps/form/VOrdenPago.java
@@ -92,6 +92,7 @@ import org.openXpertya.model.MDocType;
 import org.openXpertya.model.MPOSPaymentMedium;
 import org.openXpertya.model.MPreference;
 import org.openXpertya.model.MUser;
+import org.openXpertya.model.Query;
 import org.openXpertya.model.RetencionProcessor;
 import org.openXpertya.model.X_AD_Role;
 import org.openXpertya.model.X_C_BankAccountDoc;
@@ -1850,7 +1851,6 @@ public class VOrdenPago extends CPanel implements FormPanel,ActionListener,Table
     }
     
     private void cmdProcessActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdProcessActionPerformed
-    	
     	//Chequeo si la Entidad Comercial está bloqueada para recibir pagos
     	MBPartner partner = new MBPartner(m_ctx, (Integer)this.BPartnerSel.getValue(), m_trxName);
     	if(partner.ispaymentblocked()) { 
@@ -2055,6 +2055,28 @@ public class VOrdenPago extends CPanel implements FormPanel,ActionListener,Table
 	    		this.BPartnerSel.setValue(null);
 	    	} else {
 	    		this.cmdProcess.setEnabled(true);
+	    	}
+	    	
+	    	/*
+	    	 * Chequea que si el campo "Admitir OP Solo EC Proveedores" (del Tipo de Documento) 
+	    	 * está activo, entonces la EC debe ser un "Proveedor".
+	    	 * Por cuestiones de performance, el chequeo de si la EC es Proveedor se hace primero
+	    	 */
+	    	if(!partner.isVendor()) {
+	    		List<Object> params = new ArrayList<Object>();
+	    		final StringBuffer whereClause = new StringBuffer();
+	    		whereClause.append("isPaymentOrderSeq=? AND AllowOnlyProviders=?");
+	    		params.add(new Boolean(true));
+	    		params.add(new Boolean(true));
+	    		Query q = new Query(m_ctx, MDocType.Table_Name, whereClause.toString(), m_trxName);
+	    		q.setParameters(params);
+	    		MDocType result = q.first();
+	    		if(result != null) {
+	    			String error_msg = Msg.translate(m_ctx, "OnlyAllowedProviders");
+		    		showError(error_msg);
+		    		this.cmdProcess.setEnabled(false);//Bloqueo la continuacion del pago
+		    		this.BPartnerSel.setValue(null);
+	    		}
 	    	}
     	}
     }
