2abb7734649f4a6ff334464c35d0f758ffaf5249
mncap <mncap@c4861f4b-48fc-4c7a-83d3-e00e5e42e9c4>
2019-10-07 11:56:00 +0000

    Removed branch DE_846972_0012

--- branches/DE_846972_0012/utils/restartJBoss.sh
+++
@@ -1,9 +0,0 @@
-date ;
-cd /ServidorOXP/utils ;
-/sbin/service libertyad stop ;
-sleep 5 ;
-/ServidorOXP/utils/killEmAll.sh /ServidorOXP/jboss ;
-rm -f /var/run/libertya.pid ; 
-sleep 5 ; 
-/sbin/service libertyad start ;
-date ;
